7823f3d789aac3afe7ed71bd41e231a278c4a834
kiranalii <kiran.ali@wanclouds.net>
2019-12-02 16:44:13 +0500

    Follow standards of flask app

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
+from run import app
+
 DEBUG = False
-port = '3306'
+port = '3307'
 db_name = 'apnaschoolmysqldb'
 host = 'db'
 SECRET_KEY = "ThisisaSecret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@0.0.0.0:3307/{}'.format(db_name)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
--- run.py
+++ run.py
@@ -1,14 +1,14 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
+
 
 app = Flask(__name__)
 
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@0.0.0.0:3306/apnaschoolmysqldb'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
-# db.create_all()
 
 if __name__ == "__main__":
     from auth.api import *

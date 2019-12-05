3c6624b6d7ccd442c23397ea2d044ad17396e8cc
talhaw <talhayounus1996@gmail.com>
2019-12-05 13:16:54 +0500

    email field added to User

--- models/models.py
+++ models/models.py
@@ -6,13 +6,15 @@ from run import db
 class User(db.Model):
     __tablename__ = 'users'
     id = Column('user_id', Integer, primary_key=True)
+    email = Column('email', String(50))
     name = Column('name', String(50))
     age = Column('age', String(100))
     grade = Column('grade', Integer)
     department = Column('department', String(100))
     password = Column('password', String(100))
 
-    def __init__(self, name, age, grade, department, password):
+    def __init__(self, email, name, age, grade, department, password):
+        self.email = email
         self.name = name
         self.age = age
         self.grade = grade

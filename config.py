d12ab024169d17c642e0ddbd0afa8f41c05eb98f
talhaw <talhayounus1996@gmail.com>
2019-12-05 13:16:25 +0500

    configuration for email added in config file

--- config.py
+++ config.py
@@ -6,5 +6,12 @@ db_name = 'apnaschoolmysqldb'
 host = 'db'
 SECRET_KEY = "ThisisaSecret"
 
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@0.0.0.0:3306/{}'.format(db_name)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEFAULT_SENDER'] = 'talhayounus1996@gmail.com'
+app.config['MAIL_USERNAME'] = 'talhayounus1996@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Messi+Suarez==109'

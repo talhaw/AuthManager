b07a10ed0184cf319f84fa2d8a02c2febe342adc
talhaw <talhayounus1996@gmail.com>
2019-12-05 13:17:55 +0500

    email field added to api's. Email sendind feature added to user POST method

--- auth/api.py
+++ auth/api.py
@@ -1,25 +1,29 @@
-from flask import Flask, request, Response
+from flask import request, Response
+from flask_mail import Message
 
 from models.models import User
-from run import db, app
+from run import db, app, mail
 
 
 @app.route('/User', methods=['POST'])
 def add_user():
     result = request.form
+    email = result["email"]
     name = result["name"]
     age = result["age"]
     grade = result["grade"]
     department = result["department"]
     password = result["password"]
-    print('\nUser info = ', name, age, grade, department, password)
-    new_user = User(name, age, grade, department, password)
 
+    new_user = User(email, name, age, grade, department, password)
     db.session.add(new_user)
     db.session.commit()
 
-    response_body = {"message": "User added successfully"}
-    return response_body, 200
+    msg = Message('Confirm Email', sender=app.config['MAIL_USERNAME'], recipients=[email])
+    msg.body = 'Your have been succesfully registered to auth manager'
+    mail.send(msg)
+
+    return Response("Record added successfully", status=200)
 
 
 @app.route('/User', methods=['DELETE'])
@@ -27,11 +31,13 @@ def delete_user():
     result = request.form
     user_id = result["user_id"]
     user = db.session.query(User).filter_by(id=user_id).first()
+
     if user:
         db.session.delete(user)
-        db.commit()
-    else:
-        return Response(status=404)
+        db.session.commit()
+        return Response(status=200)
+
+    return Response(status=404)
 
 
 @app.route('/User', methods=['GET'])
@@ -45,30 +51,23 @@ def get_user():
     db.session.close()
 
     if quer:
-        response_body = {"message": "User exists with this id"}
-        # return response_body, 200
         return Response(status=200)
 
-    else:
-        return Response(status=404)
+    return Response(status=404)
 
 
 @app.route('/User', methods=['PUT'])
 def update_user():
     result = request.form
     user_id = result["user_id"]
-    return Response(status=200)
+    name = result["name"]
 
-    # quer = db.session.query(User).filter_by(id=user_id).first()
-    # # db.session.commit()
-    # # db.session.close()
-    #
-    # if quer:
-    #     quer.name = "talha"
-    #     # quer = db.session.query(User).filter_by(id=user_id).update(name=result["name"])
-    #     db.session.commit()
-    #     db.session.close()
-    #     return Response(status=200)
-    #
-    # else:
-    #     return Response(status=404)
+    quer = db.session.query(User).filter_by(id=user_id).first()
+    if quer:
+        quer.name = name
+        db.session.commit()
+        db.session.close()
+        return Response(status=200)
+
+    else:
+        return Response(status=404)

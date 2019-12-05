ea7482b4e061a7950fe3f0749d595516b2149bc9
talhaw <talhayounus1996@gmail.com>
2019-12-05 13:15:55 +0500

    configuration for email added

--- run.py
+++ run.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEFAULT_SENDER'] = 'talhayounus1996@gmail.com'
+app.config['MAIL_USERNAME'] = 'talhayounus1996@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Messi+Suarez==109'
 app.config.from_pyfile('config.py')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@0.0.0.0:3306/apnaschoolmysqldb'
+mail = Mail(app)
 
 db = SQLAlchemy(app)
+
 
 if __name__ == "__main__":
     from auth.api import *
